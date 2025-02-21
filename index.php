ae7cf0faa44592464469066a15bb1e9f5f2e97dc
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2025-02-21 14:43:54 +0200

    Adding index.php to act as API entry point and route the HTTP requests internally
    The index.php file will serve as router to direct incoming requests to the appropriate functions, which will then handle the data aggregation and send a response to users

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-echo "API connection established successfully";
+<?php
+
+// This is the entry point for the API and routes the HTTP requests
+header('Content-Type: application/json');
+
+echo json_encode(['message' => 'API connection successful']);
